1b06caf59b829f108fb6e4a8cbc766115750f5bd
Greg Hewgill <greg@hewgill.com>
2009-09-09 08:51:58 +0000

    update paths to be self-contained

--- posts.sh
+++ posts.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-#grep '<question' unify.xml | cut -d\" -f2 | xargs -L 1 -I{} xt unify/{}.xml posts.xsl
-javac -cp /home/greg/build/xt20051206.jar posts.java
-java -cp .:/home/greg/build/xt20051206.jar:/home/greg/build/xp/xp.jar posts
+javac -cp xt20051206.jar posts.java
+java -cp .:xt20051206.jar:xp.jar posts
